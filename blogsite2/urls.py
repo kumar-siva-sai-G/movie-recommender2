a65a09912156690b83aac4def35ca82960e1e569
Kumar Siva Sai Gourabathuni <kumarsivasaigourabathuni@gmail.com>
2025-04-06 09:18:09 +0530

    Update urls.py

--- blogsite2/urls.py
+++ blogsite2/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from django.views.generic import TemplateView
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 urlpatterns = [
     path('admin/', admin.site.urls),
      path('', TemplateView.as_view(template_name='index.html'), name='home'),
